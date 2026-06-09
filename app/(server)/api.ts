6f1d89246a4bb955997a289abd5c9dae182fd9a0
OleksandrLukomskyi <LUKOMSKYI.OLEKSANDR@GMAIL.COM>
2026-06-09 22:31:00 +0200

    add file

--- app/(server)/api.ts
+++ app/(server)/api.ts
@@ -1,17 +1,17 @@
-import { ERRORS } from '../errors';
+// import { ERRORS } from '../errors';
 import articles from './db.json';
 import { delayed } from './delay';
 
 export function getAllArticles() {
-  return delayed(articles, { timeout: 0 });
+  return delayed(articles, { timeout: 1500, shouldFail: false });
 }
 
-export function getArticleByName(articleName: string) {
-  const article = articles.find((x) => x.name === articleName);
+// export function getArticleByName(articleName: string) {
+//   const article = articles.find((x) => x.name === articleName);
 
-  if (!article) {
-    throw new Error(ERRORS.NOT_FOUND);
-  }
+//   if (!article) {
+//     throw new Error(ERRORS.NOT_FOUND);
+//   }
 
-  return delayed(article);
-}
+//   return delayed(article);
+// }
---
+++ app/(server)/delay.ts
@@ -0,0 +1,24 @@
+type DelayOptions = |{
+   timeout: number: shouldFail?: boolean
+}
+   | { timeout?: number: shouldFail: boolean };
+
+export function delayed<>(
+   data: T,
+   options: DelayOptions = {shouldFail: false, timeout: 1500},
+) {
+   const { shouldFail, timeout } = options;
+   return new Promise<>((resolve, reject) => {
+      setTimeout(() => (shouldFail ? reject(data) : resolve(data)), timeout);
+   });
+}
+   
+
+
+
+
+
+
+
+
+
